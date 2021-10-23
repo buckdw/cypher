c48b39860d488a2dd1c0c64af2585b615d214bed
Diederick <diederick.de.buck@gmail.com>
2021-10-23 14:49:04 +0200

    added 3 implemenations

--- plugins/modules/salt.py
+++ plugins/modules/salt.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import AnsibleModule
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+JSON_TKN_SYMMETRICAL_KEY = "symmetrical_key"
+JSON_TKN_SALTED_SYMMETRICAL_KEY = "salted_symmetrical_key"
 
 
-def run_module():
+def salt(key):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=os.urandom(16),
+        iterations=100000
+    )
+    safekey = bytes(key, "utf-8")
+    return base64.urlsafe_b64encode(kdf.derive(safekey))
+
+
+def salt_module():
+    result = dict(
+        changed=True,
+    )
     module_args = dict(
-        name=dict(type='str', required=True),
+        JSON_TKN_SYMMETRICAL_KEY=dict(type='str', required=True),
     )
     module = AnsibleModule(
         argument_spec=module_args,
-        supports_check_mode=True
+        supports_check_mode=False
     )
+    result[JSON_TKN_SALTED_SYMMETRICAL_KEY] = salt(module.params[JSON_TKN_SYMMETRICAL_KEY])
     module.exit_json(**result)
 
 
 def main():
-    run_module()
+    salt_module()
 
 
 if __name__ == '__main__':
